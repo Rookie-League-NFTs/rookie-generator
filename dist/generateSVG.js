588bf0160fb69c5f27fc04f52cbd144609ac1a3f
Sébastien Audet <seb.audet26@gmail.com>
2021-09-05 16:01:35 -0400

    lets goooo

--- dist/generateSVG.js
+++ dist/generateSVG.js
@@ -3,8 +3,6 @@ exports.__esModule = true;
 exports.generateSVG = void 0;
 var template_1 = require("./template");
 var fs_1 = require("fs");
-// variable for the namespace 
-var svgns = "http://www.w3.org/2000/svg";
 function generateSVG() {
     // const test: SVGElement = document.createElement("svg");
     var test = template_1.template.svg();
--- generateSVG.ts
+++ generateSVG.ts
@@ -1,19 +1,15 @@
 import { template } from "./template";
 import { writeFileSync } from "fs";
 
+
 interface params {
     height: string,
     weight: string
 }
 
-// variable for the namespace 
-const svgns = "http://www.w3.org/2000/svg";
-
-
 export function generateSVG(): SVGElement {
     // const test: SVGElement = document.createElement("svg");
     const test = template.svg();
-    
     const output = writeFileSync('./output/test.svg', template.svg())
 
     return template;
